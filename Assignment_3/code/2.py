8a49f1e975fa2fd12af7eeb128c39ff017821f55
memelordmaddy <madhavrbabu@gmail.com>
2024-10-15 01:05:39 +0530

    Finished Q2

--- Assignment_3/code/2.py
+++ Assignment_3/code/2.py
@@ -9,42 +9,47 @@ class EpanechnikovKDE:
 
     def fit(self, data):
         """Fit the KDE model with the given data."""
-        self.data= np.array(data)
+        self.data = np.array(data)
 
     def epanechnikov_kernel(self, x, xi):
         """Epanechnikov kernel function."""
-        y= (x-xi)/self.bandwidth
-        if(np.abs(y)<=1):
-            return 0.75 * (1-y**2)/ self.bandwidth
-        else:
-            return 0
+        distances_squared = np.sum(((x - xi) / self.bandwidth)**2, axis=1)
+        condition = distances_squared <= 1
+        return (2 / np.pi) * (1 - distances_squared) * condition
 
     def evaluate(self, x):
-        """Evaluate the KDE at point x."""
-        x = np.array(x)[:, np.newaxis]  # Add a new axis to x
-        return np.mean(self.epanechnikov_kernel(x, self.data), axis=1)
+        """Evaluate the KDE at multiple points x."""
+        return np.sum(self.epanechnikov_kernel(x, self.data)) / (len(self.data) * self.bandwidth**2)
+
 
 
 # Load the data from the NPZ file
 data_file = np.load('transaction_data.npz')
 data = data_file['data']
-print(data.shape)
 
 # TODO: Initialize the EpanechnikovKDE class
-kde = EpanechnikovKDE(bandwidth=1.0)
+bandwidth = 1.0 
+kde = EpanechnikovKDE(bandwidth=bandwidth)
 
 # TODO: Fit the data
 kde.fit(data)
 
 # TODO: Plot the estimated density in a 3D plot
+x_min, x_max = data[:, 0].min(), data[:, 0].max()
+y_min, y_max = data[:, 1].min(), data[:, 1].max()
+x = np.linspace(x_min, x_max, 100)
+y = np.linspace(y_min, y_max, 100)
+X, Y = np.meshgrid(x, y)
+grid_points = np.c_[X.ravel(), Y.ravel()]
+Z = np.array([kde.evaluate(point) for point in grid_points]).reshape(X.shape)
+fig = plt.figure(figsize=(12, 8))
+axis = fig.add_subplot(111, projection='3d')
+axis.plot_surface(X, Y, Z, cmap='plasma')
+axis.set_title('Kernal Density 3D plot')
+axis.set_xlabel('X')
+axis.set_ylabel('Y')
+axis.set_zlabel('KDE')
+
 # TODO: Save the plot 
-x_k= np.linspace(data.min(), data.max(), 1000)
-y_k= kde.evaluate(x_k)
-plt.figure(figsize=(10, 6))
-plt.hist(data, bins=30, density=True, alpha=0.7, color='skyblue', edgecolor='black')
-plt.plot(x_k, y_k, 'r-', lw=2)
-plt.title('Epanechnikov Kernel Density Estimation')
-plt.xlabel('Value')
-plt.ylabel('Density')
-plt.grid(True, alpha=0.3)
+plt.savefig('transaction_distribution.png')
 plt.show()
--- Assignment_3/code/dummy.py
+++ Assignment_3/code/dummy.py
@@ -1,86 +1,62 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class EpanechnikovKDE:
-    def __init__(self, bandwidth=1.0, sample_size=5000):
+    def __init__(self, bandwidth=1.0):
         self.bandwidth = bandwidth
         self.data = None
-        self.sample_size = sample_size
 
     def fit(self, data):
         """Fit the KDE model with the given data."""
-        if len(data) > self.sample_size:
-            indices = np.random.choice(len(data), self.sample_size, replace=False)
-            self.data = data[indices]
-        else:
-            self.data = np.array(data)
+        self.data = np.array(data)
 
-    def calculate_distances(self, points, batch_size=1000):
-        """Calculate pairwise distances between points and self.data in batches."""
-        n_points = len(points)
-        distances = np.zeros((len(self.data), n_points))
-        
-        for i in range(0, n_points, batch_size):
-            batch = points[i:i+batch_size]
-            batch_distances = np.sqrt(((self.data[:, np.newaxis, :] - batch[np.newaxis, :, :])**2).sum(axis=2))
-            distances[:, i:i+batch_size] = batch_distances
-        
-        return distances
-
-    def epanechnikov_kernel(self, distances):
+    def epanechnikov_kernel(self, x, xi):
         """Epanechnikov kernel function."""
-        scaled_distances = distances / self.bandwidth
-        return np.where(scaled_distances <= 1, 
-                        0.75 * (1 - scaled_distances**2) / (np.pi * self.bandwidth**2), 
-                        0)
+        distances_squared = np.sum(((x - xi) / self.bandwidth)**2, axis=1)
+        condition = distances_squared <= 1
+        return (2 / np.pi) * (1 - distances_squared) * condition
 
-    def evaluate(self, points):
-        """Evaluate the KDE at given points."""
-        distances = self.calculate_distances(points)
-        return np.mean(self.epanechnikov_kernel(distances), axis=0)
+    def evaluate(self, x):
+        """Evaluate the KDE at multiple points x."""
+        return np.sum(self.epanechnikov_kernel(x, self.data)) / (len(self.data) * self.bandwidth**2)
 
-import numpy as np
-import matplotlib.pyplot as plt
 
 # Load the data from the NPZ file
 data_file = np.load('transaction_data.npz')
 data = data_file['data']
+print(data.shape)
 
-# Initialize the EpanechnikovKDE class
-kde = EpanechnikovKDE(bandwidth=1.0, sample_size=5000)
+bandwidth = 1.0  # You may need to adjust this value
+kde = EpanechnikovKDE(bandwidth=bandwidth)
 
 # Fit the data
 kde.fit(data)
+# Define the range for evaluation
+x_min, x_max = data[:, 0].min(), data[:, 0].max()
+y_min, y_max = data[:, 1].min(), data[:, 1].max()
 
-# Create a grid of points (reduced resolution)
-x_range = np.linspace(data[:, 0].min(), data[:, 0].max(), 50)
-y_range = np.linspace(data[:, 1].min(), data[:, 1].max(), 50)
-xx, yy = np.meshgrid(x_range, y_range)
-grid_points = np.column_stack([xx.ravel(), yy.ravel()])
+# Create a grid of points
+x = np.linspace(x_min, x_max, 100)
+y = np.linspace(y_min, y_max, 100)
+X, Y = np.meshgrid(x, y)
+grid_points = np.c_[X.ravel(), Y.ravel()]
 
-# Evaluate KDE on the grid
-z = kde.evaluate(grid_points).reshape(xx.shape)
+# Evaluate KDE on the grid (vectorized)
+Z = np.array([kde.evaluate(point) for point in grid_points]).reshape(X.shape)
 
-# Plot the estimated density in a 3D plot
+# Create a 3D plot
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(xx, yy, z, cmap='viridis', edgecolor='none')
+
+# Plot the surface
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Add labels and title
+ax.set_title('3D Probability Density of Transactions')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Density')
-ax.set_title('2D Epanechnikov Kernel Density Estimation')
-fig.colorbar(surf, shrink=0.5, aspect=5)
 
 # Save the plot
-plt.savefig('2d_epanechnikov_kde_plot.png')
-plt.show()
-
-# Create a 2D contour plot
-plt.figure(figsize=(10, 8))
-plt.contourf(xx, yy, z, levels=20, cmap='viridis')
-plt.colorbar(label='Density')
-plt.scatter(data[:, 0], data[:, 1], alpha=0.1, color='red', s=1)
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('2D Epanechnikov KDE Contour Plot')
-plt.savefig('2d_epanechnikov_kde_contour.png')
+plt.savefig('transaction_distribution_3d.png')
 plt.show()
