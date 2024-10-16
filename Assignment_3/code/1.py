cb17a3f0aafcfb63d33bdace2f600aa1007819b0
Pranay Marumamula <53103601+mvpranay@users.noreply.github.com>
2024-10-16 22:39:52 +0530

    added 1.py ass 3 question 1

---
+++ Assignment_3/code/1.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# pass the path to data.csv 
+df = pd.read_csv('data.csv', usecols=[6], nrows=1500, header=13)
+
+data = df.iloc[:, 0].to_numpy()
+data = data[data <= 4]
+
+counts = plt.hist(data, bins=np.linspace(0, 4, 11))[0]
+plt.title("Filtered data")
+plt.xlabel("Reading")
+plt.ylabel("Number of readings")
+plt.savefig("10binhistogram.png")
+
+total_count = np.sum(counts)
+probability = counts / total_count
+
+print(probability)
+
+def cross_validation_score(data, n):
+
+    x = []
+    y = []
+    for nbins in range(1, 1001):
+        h = 4 / nbins
+
+        _bins = np.linspace(0, 4, nbins + 1)
+        count = np.histogram(data, _bins)[0]
+        probability = (count / n)
+
+        pj_2 = np.sum(probability ** 2)
+
+        y_h = 2/((n - 1) * h) - ((n + 1) * pj_2)/((n - 1) * h)
+        x.append(h)
+        y.append(y_h)
+
+    plt.clf()
+    plt.plot(x, y)
+    plt.title("Cross Validation Score Vs Bandwidth")
+    plt.ylabel("Cross Validation Score")
+    plt.xlabel("Bandwidth(h)")
+    plt.savefig("crossvalidation.png")
+
+    h_min = x[np.argmin(y)]
+    
+    return h_min
+
+h_min = cross_validation_score(data, total_count)
+
+plt.clf()
+counts = plt.hist(data, bins=np.linspace(0, 4, int(4/h_min)+1))[0]
+plt.title("Optimal Histogram")
+plt.ylabel("Frequency")
+plt.xlabel("Reading")
+plt.savefig("optimalhistogram.png")
